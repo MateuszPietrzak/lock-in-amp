b6c4c83dc151dd7f9f626f1f11034dec3e5f2799
Mateusz Pietrzak <mateusz.jerzy.pietrzak@gmail.com>
2026-04-04 14:10:17 +0200

    Add iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    Element,
+    widget::{button, column, text},
+};
+
+pub fn main() -> iced::Result {
+    #[cfg(target_arch = "wasm32")]
+    {
+        console_log::init().expect("Initialize logger");
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    tracing_subscriber::fmt::init();
+
+    iced::application(App::default, App::update, App::view)
+        .title("Lock In Amp")
+        .centered()
+        .run()
+}
+
+#[derive(Default)]
+struct App {
+    value: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl App {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        column![
+            button("+").on_press(Message::Increment),
+            text(self.value),
+            button("-").on_press(Message::Decrement),
+        ]
+        .into()
+    }
 }
